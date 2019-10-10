962b3559e5f8e6033d7ac9f651e7be6d92eb7548
HigorGomesLima <37384951+HigorGomesLima@users.noreply.github.com>
2019-10-10 18:49:46 -0300

    cadastro turma
    gerar lista de acordo com csv

--- Projeto/js/cadastro-turma.js
+++ Projeto/js/cadastro-turma.js
@@ -31,25 +31,41 @@ function lerArquivoCSV(){
 function loadCSV(event){
     var csv = event.target.result;
     var lines = csv.split('\n');
-    var inicio = 0;
-    var listaAlunos;
+    var inicio = 999;
+    var listaAlunos = [];
+    var dados = true;
     lines.forEach((line,i) => {
-        console.log(i,line);
         var texto = line.split(';');
         texto.forEach((text) => {
             if(text == 'Nome do Aluno'){
-                inicio = i+1;
+                inicio = i + 1;
             }
         });
-        if(i >= inicio){
-            var aluno;
-            texto.forEach((text) => {
-                if(text != ''){
-                    aluno.push(text);
-                }
-            })
+    });
+    for(var i = inicio;dados;i++){
+        var linha = lines[i].split(';');
+        if(linha.length < 3) dados = false
+        else {
+            linha = linha.filter(e => e !== '');
+            var aluno = {
+                matricula: linha[0],
+                nome: linha[1]
+            };
             listaAlunos.push(aluno);
         }
+    }
+    criarTabelaAlunos(listaAlunos);
+}
+
+function criarTabelaAlunos(lista){
+    var _tabela_aluno = document.querySelector(".formulario-aluno .tabela-aluno tbody");
+    var _padrao = document.querySelectorAll(".formulario-aluno .tabela-aluno tbody .dados-aluno td");
+    lista.forEach((aluno) => {
+        var nova_linha = _tabela_aluno.insertRow(-1);
+        nova_linha.insertCell(0).innerHTML = '<td>'+aluno.matricula+"</td>"
+        nova_linha.insertCell(1).innerHTML = '<td>'+aluno.nome+"</td>"
+        nova_linha.insertCell(2).innerHTML = _padrao[2].innerHTML;
+        nova_linha.insertCell(3).innerHTML = _padrao[3].innerHTML;
+        $(".btn-excluir").bind("click",removerCampo);
     });
-    console.log(listaAlunos);
 }
